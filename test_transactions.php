9a210d1d150e3e69abd5a1e68828793e2ad8887e
VaySagab <acclords11bol@gmail.com>
2025-11-21 10:27:59 +0700

    first

--- test_transactions.php
+++ test_transactions.php
@@ -5,7 +5,7 @@ require_once 'includes/function.php';
 
 // Test langsung insert ke database
 $test_sql = "INSERT INTO transaksi (id_kas, jenis, jumlah, tanggal, deskripsi, created_by) 
-             VALUES (1, 'pemasukan', 10000, '2025-11-20', 'Test langsung', 1)";
+             VALUES (100, 'pemasukan', 10000, '2025-11-20', 'Test langsung', 1)";
 
 if (mysqli_query($conn, $test_sql)) {
     echo "SUCCESS: Test transaction inserted. ID: " . mysqli_insert_id($conn);
